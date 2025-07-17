85a5f7a748ac9f4a1c3205ee4c6dd871de6855fa
hanep <62378693+hanepudding@users.noreply.github.com>
2025-07-17 15:27:06 +0800

    1.0.0 added Python

--- generate_json.py
+++ generate_json.py
@@ -24,8 +24,10 @@ def generate_icons_json(icons_dir, output_file, base_url, json_name="HanepIcons"
     with open(output_file, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
+from config_manager import ConfigManager as c
+
 if __name__ == "__main__":
-    ICONS_DIRECTORY = "D:/Workspace/git-repos/icon-library/Icons"
-    OUTPUT_JSON = "D:/Workspace/git-repos/icon-library/HanepIcons.json"
+    ICONS_DIRECTORY = c.ws() / "git-repo/icon-library/Icons"
+    OUTPUT_JSON = c.ws() / "git-repo/icon-library/HanepIcons.json"
     BASE_URL = "https://raw.githubusercontent.com/hanepudding/icon-library/master/Icons"
     generate_icons_json(ICONS_DIRECTORY, OUTPUT_JSON, BASE_URL)
